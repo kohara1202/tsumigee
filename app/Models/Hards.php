2e98a95a17789200abc5150c20a67e329e05ff78
kohara1202 <kohara1202@gmail.com>
2022-12-29 13:48:32 +0900

    Gamecontroller_ログイン機能

---
+++ app/Models/Hards.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Hards extends Model
+{
+    use HasFactory;
+}
